c4370149a8c4da61a377e7f73f2890ecd624fd3b
WailingSpud <sebastian.montoto@rai.usc.es>
2026-04-06 10:58:13 +0200

    Añadido el proyecto Java Maven

---
+++ ParqueNatural/src/main/java/com/mycompany/parquenatural/ParqueNatural.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parquenatural;
+
+/**
+ *
+ * @author alumnogreibd
+ */
+public class ParqueNatural {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
